fd4c857132ba8f5f6a43ba14a731222c3426221d
denglixi <denglixi.cs@gmail.com>
2019-11-22 15:36:28 +0800

    multiprocess by concurrent.feature

--- attribute.py
+++ attribute.py
@@ -4,12 +4,18 @@ import json
 def read_attribute_from_csv(attribute_path):
     attribute_data = pd.read_csv(attribute_path, error_bad_lines=False, encoding='utf-8')
     # id,source,food name,entity,shape,taste,cooking method,other,type
-    attr_entity = list(set(attribute_data['entity']))
-    attr_shape = list(set(attribute_data['shape']))
-    attr_taste = list(set(attribute_data['taste']))
-    attr_cook = list(set(attribute_data['cooking method']))
-    attr_other = list(set(attribute_data['other']) | {'大', '小', '大份', '小份', '大杯', '小杯', '(', ')', '（', '）'})
-    attr_type = list(set(attribute_data['type']))
+    # attr_entity = list(set(attribute_data['entity']))
+    # attr_shape = list(set(attribute_data['shape']))
+    # attr_taste = list(set(attribute_data['taste']))
+    # attr_cook = list(set(attribute_data['cooking method']))
+    # attr_other = list(set(attribute_data['other']) | {'大', '小', '大份', '小份', '大杯', '小杯', '(', ')', '（', '）'})
+    # attr_type = list(set(attribute_data['type']))
+    attr_entity = set(attribute_data['entity'])
+    attr_shape = set(attribute_data['shape'])
+    attr_taste = set(attribute_data['taste'])
+    attr_cook = set(attribute_data['cooking method'])
+    attr_other = set(attribute_data['other']) | {'大', '小', '大份', '小份', '大杯', '小杯', '(', ')', '（', '）'}
+    attr_type = set(attribute_data['type'])
     attributes = {'entity': attr_entity,
                   'shape': attr_shape,
                   'taste': attr_taste,
--- entity_match.py
+++ entity_match.py
@@ -8,7 +8,7 @@ from attribute import read_attribute_from_csv
 from text_search import text_search
 from visulization import draw_hist
 import jieba
-from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import ProcessPoolExecutor, as_completed
 import time
 import math
 
@@ -28,11 +28,10 @@ def entity_match(source_entities, target_entities_list, attributes):
     max_matched_score = 0
     matched_target_idx = -1
     matched_target_str = ''
-
     attribute_uncount_set = attributes['other'] | attributes['shape'] | attributes['taste'] | attributes['type'] | \
                             attributes['cooking method']
     # Debug
-    # source_str = "".join(source_entities)
+    source_str = "".join(source_entities)
     # if not source_str == '凉瓜牛肉饭':
     #     return 0, 0, ''
 
@@ -87,7 +86,7 @@ def entity_match(source_entities, target_entities_list, attributes):
             matched_target_idx = target_i
             matched_target_str = "".join(target_entities)
 
-    return max_matched_score, matched_target_idx, matched_target_str,
+    return max_matched_score, matched_target_idx, matched_target_str, source_str
 
 
 def all_in_set(sources, targets):
@@ -119,24 +118,23 @@ def find_unmatched_entities(sources, targets):
     return unmatched_entities
 
 
-def main():
-
+def multiprocess_main():
     # Initialization
-    #menu
+    # menu
     menu_path = './sources/sample_menu_cleaned2.csv'
     menu_data = pd.read_csv(menu_path, error_bad_lines=False, encoding='utf-8')
-    #attribute
+    # attribute
     attribute_path = './sources/recipe_attribute.csv'
     add_voc_by_attribute(attribute_path)
     attributes = read_attribute_from_csv(attribute_path)
-    #recipe
-    recipe_jieba_path = './sources/recipe_name_jieba.xlsx'
-    # recipe_jieba_path = './sources/recipe_name_jieba_exclude_meishijie.xlsx'
+    # recipe
+    #recipe_jieba_path = './sources/recipe_name_jieba.xlsx'
+    recipe_jieba_path = './sources/recipe_name_jieba_exclude_meishijie.xlsx'
     recipe_jieba_data = load_recipe_jieba_date(recipe_jieba_path)
     # get the jieba results
     recipe_jieba_data = [x[1].split('/') for x in recipe_jieba_data]
     recipe_jieba_data = recipe_jieba_data[1:]
-    #saving setting
+    # saving setting
     recipe_data = recipe_jieba_data
     suffix_str = 'exclude_useless_attri'
     recipe_name = 'jieba_' \
@@ -151,58 +149,66 @@ def main():
     useless_record_file = open('outputs/match_record_{}_useless.csv'.format(recipe_name), 'w', encoding='utf-8')
     style_record_file = open('outputs/match_record_{}_style.csv'.format(recipe_name), 'w', encoding='utf-8')
     ingredient_record_file = open('outputs/match_record_{}_ingredient.csv'.format(recipe_name), 'w', encoding='utf-8')
-    #visualization
+    # visualization
     matched_weighted_result = []
-    #multi processing setting
+    # multi processing setting
     procsss_pool = ProcessPoolExecutor()
-
-
-    # processing matching
+    fs_list = []
+    # set up multiprocess pool
     for menu_idx, menu in menu_data.iterrows():
-        if menu_idx % 20 == 0:
+        if menu_idx % 200 == 0:
             print(menu_idx)
         try:
             # try exception for some menu are wrong
             menu_text = menu['name']
             menu_text_splited = list(jieba.cut(menu_text, cut_all=False))
-            matched_score, matched_target_idx, matched_target_str = entity_match(menu_text_splited,
-                                                                                 recipe_jieba_data,
-                                                                                 attributes)
-            unmatched_entities = set(find_unmatched_entities(menu_text, matched_target_str))
-            matched_weighted_result.append(matched_score)
-            record_str = "{}\t{}\t{:.2}\n".format(
-                menu['name'],
-                matched_target_str,
-                matched_score
-            )
-            # write outputs to different record files
-            if len(unmatched_entities) == 0:
-                totalmatched_record_file.write(record_str)
-                totalmatched_record_file.flush()
-            elif all_in_set(unmatched_entities, attributes['type']):
-                type_record_file.write(record_str)
-                type_record_file.flush()
-            elif all_in_set(unmatched_entities, attributes['cooking method'] | attributes['shape']):
-                cooking_record_file.write(record_str)
-                cooking_record_file.flush()
-            elif all_in_set(unmatched_entities, attributes['taste'] | attributes['other']):
-                style_record_file.write(record_str)
-                style_record_file.flush()
-            elif all_in_set(unmatched_entities,
-                            attributes['other'] | attributes['shape'] | attributes['taste'] | attributes['type'] |
-                            attributes['cooking method']):
-                useless_record_file.write(record_str)
-                useless_record_file.flush()
-            else:
-                ingredient_record_file.write(record_str)
-                ingredient_record_file.flush()
-            record_file.write(record_str)
-            record_file.flush()
+            fs_list.append(procsss_pool.submit(entity_match,
+                                               menu_text_splited,
+                                               recipe_jieba_data,
+                                               attributes))
         except Exception as e:
             print(e)
+    # get results from pool
+    fs_count = 0
+    for feature in as_completed(fs_list):
+        fs_count += 1
+        if fs_count % 20 == 0:
+            print(fs_count)
+        matched_score, matched_target_idx, matched_target_str, menu_text = feature.result()
+        unmatched_entities = set(find_unmatched_entities(menu_text, matched_target_str))
+        matched_weighted_result.append(matched_score)
+        record_str = "{}\t{}\t{:.2}\n".format(
+            menu_text,
+            matched_target_str,
+            matched_score
+        )
+        # write outputs to different record files
+        if len(unmatched_entities) == 0:
+            totalmatched_record_file.write(record_str)
+            totalmatched_record_file.flush()
+        elif all_in_set(unmatched_entities, attributes['type']):
+            type_record_file.write(record_str)
+            type_record_file.flush()
+        elif all_in_set(unmatched_entities, attributes['cooking method'] | attributes['shape']):
+            cooking_record_file.write(record_str)
+            cooking_record_file.flush()
+        elif all_in_set(unmatched_entities, attributes['taste'] | attributes['other']):
+            style_record_file.write(record_str)
+            style_record_file.flush()
+        elif all_in_set(unmatched_entities,
+                        attributes['other'] | attributes['shape'] | attributes['taste'] | attributes['type'] |
+                        attributes['cooking method']):
+            useless_record_file.write(record_str)
+            useless_record_file.flush()
+        else:
+            ingredient_record_file.write(record_str)
+            ingredient_record_file.flush()
+        record_file.write(record_str)
+        record_file.flush()
+
     print(matched_weighted_result)
     draw_hist(matched_weighted_result, recipe_name)
 
-
 if __name__ == '__main__':
-    main()
+    #main()
+    multiprocess_main()
